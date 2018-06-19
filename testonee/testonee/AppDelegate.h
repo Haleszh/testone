dd4c0c73f8a804512ae22b71c106eb5299191b05
klgzwork001 <zhanghao@kuailegezi.com>
2018-06-19 18:38:03 +0800

    no message

---
+++ testonee/testonee/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testonee
+//
+//  Created by klgzwork001 on 2018/6/19.
+//  Copyright © 2018年 klgzwork001. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
